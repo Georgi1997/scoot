d438c5b0e181f07c35eed51ec54ccca6f2069962
Georgi1997 <valkovgeorgi1997@gmail.com>
2021-03-29 15:57:25 +0300

    reveal on scroll js

--- js/app.js
+++ js/app.js
@@ -37,3 +37,27 @@ const toggleFaq = () => {
 };
 
 toggleFaq();
+
+//reveal on scroll
+
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.classList.remove("section--hidden");
+  }
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
